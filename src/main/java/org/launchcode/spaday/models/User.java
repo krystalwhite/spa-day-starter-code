857ffe71c9fedd93c931adc2d4ce268b6d01de85
krystalwhite <krystalsnowhite@gmail.com>
2021-09-16 14:04:50 -0500

    commented out non-functioning bonus

--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -4,13 +4,13 @@ public class User {
     private String username;
     private String email;
     private String password;
-    private int id;
+//    private int id;
 
-    public User(String username, String email, String password, int id) {
+    public User(String username, String email, String password) {
         this.username = username;
         this.email = email;
         this.password = password;
-        this.id = id;
+//        this.id = id;
     }
 
     public String getUsername() {
@@ -37,11 +37,11 @@ public class User {
         this.password = password;
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public void setId(int id) {
+//        this.id = id;
+//    }
 }
