97f7a36f19caf24c5ab0b775c7120e632eea9e30
krystalwhite <krystalsnowhite@gmail.com>
2021-09-15 20:49:53 -0500

    12.6.5 part 1

--- src/main/java/org/launchcode/spaday/controllers/UserController.java
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -1,8 +1,9 @@
 package org.launchcode.spaday.controllers;
 
+import org.launchcode.spaday.models.User;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("user")
@@ -11,7 +12,16 @@ public class UserController {
 
     @GetMapping("add")
     public String displayAddUserForm() {
-
         return "user/add";
+    }
+
+    @PostMapping
+    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
+//        if (user.getPassword() == verify) {
+            model.addAttribute("username", user.getUsername());
+            return "user/index";
+//        } else {
+//            return "user/add";
+//        }
     }
 }
